7ebc04d84f3747477b06cfc42641ee025fec667c
TKR-503 <tawhidrafe2050@gmail.com>
2026-09-06 02:18:59 +0600

    Change dot env file in the server

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     console.log("Successfully connected to MongoDB!");
 
     const database = client.db("Drivefleet_Car_Platform");
